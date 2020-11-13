35afe358373331b2f55cfa4ac9cb0a3c14bada55
Ryan King <ryanking@chapman.edu>
2020-11-13 10:54:46 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,10 +3,12 @@
 # Ryan King
 echo "Enter a username : "
 read -r  NAME
-while echo "$NAME" | egrep -E -v "^[a-z]{5}$" > /dev/null 2>&1
+while echo "$NAME" | egrep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter only lowercase letters, digits, and the underscore 	character!"
+	echo "You must have it start with a lower case letter"
+	echo "You must cotain at least three but no more than 12 characters" 
+	echo "Enter a username: "
 	read -r  NAME
 done
 echo "Thank you"
